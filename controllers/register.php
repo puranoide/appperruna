8e6ec2ba82cb54467157a43906b2379d26ed069d
gabriel acosta sanchez <121548953+puranoide@users.noreply.github.com>
2026-03-08 00:11:43 -0500

    Update register.php

--- controllers/register.php
+++ controllers/register.php
@@ -110,7 +110,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             }
             break;
         case 'mandarcorreo':
-            $response = mandarcorreo($data);
+            $response = enviarCorreoInteresado($data);
             if (isset($response['error'])) {
                 echo json_encode(['error' => $response['error']]); // ← verás el error exacto
             } else {
